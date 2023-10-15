e07673b4a0b066b75c426c7ae827f7e0c4be62ad
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-10-15 11:39:33 +0200

    Added greetingText

--- app/src/main/java/com/hogent/androidproject/MainActivity.kt
+++ app/src/main/java/com/hogent/androidproject/MainActivity.kt
@@ -3,13 +3,20 @@ package com.hogent.androidproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hogent.androidproject.ui.theme.AndroidprojectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingText()
                 }
             }
         }
@@ -30,17 +37,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,verticalArrangement = Arrangement.spacedBy(5.dp), modifier = modifier.padding(3.dp)) {
+        Text(
+            text = stringResource(R.string.welcome),
+            modifier = modifier
+        )
+        Text(
+            text = stringResource(R.string.platform_selection),
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidprojectTheme {
-        Greeting("Android")
+        GreetingText()
     }
 }

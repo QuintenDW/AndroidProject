758d2fdc717ac23d1fba6093f97cacca83c9dae4
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-10-20 16:31:23 +0200

    Added button to startScreen

--- app/src/main/java/com/hogent/androidproject/ui/components/StartScreenComponent.kt
+++ app/src/main/java/com/hogent/androidproject/ui/components/StartScreenComponent.kt
@@ -2,10 +2,14 @@ package com.hogent.androidproject.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.hogent.androidproject.GreetingText
+import com.hogent.androidproject.R
 
 @Composable
 fun StartScreen(modifier: Modifier = Modifier, platformOptions: List<String>, selectedOption: String, onOptionChange: (String) -> Unit ) {
@@ -15,5 +19,9 @@ fun StartScreen(modifier: Modifier = Modifier, platformOptions: List<String>, se
     ) {
         GreetingText()
         OptionsList(options = platformOptions , selectedOption = selectedOption, onOptionChange = onOptionChange)
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.volgende))
+        }
     }
 }

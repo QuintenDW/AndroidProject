59cf1eea70a314c3958dffd16b3ede85ca9ef188
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-10-21 11:54:12 +0200

    Added title to enums

--- app/src/main/java/com/hogent/androidproject/ui/NavigationRoutes.kt
+++ app/src/main/java/com/hogent/androidproject/ui/NavigationRoutes.kt
@@ -1,8 +1,11 @@
 package com.hogent.androidproject.ui
 
-enum class NavigationRoutes() {
-    Start,
-    Category,
-    List,
-    About,
+import androidx.annotation.StringRes
+import com.hogent.androidproject.R
+
+enum class NavigationRoutes(@StringRes val title: Int) {
+    Start(title = R.string.start),
+    Category(title = R.string.categorie),
+    List(title = R.string.overzicht),
+    About(title = R.string.over),
 }

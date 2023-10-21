63b6895d91af04ba3b3302e76449e80dc3efbdee
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-10-21 16:25:09 +0200

    Added datasource to setup list

---
+++ app/src/main/java/com/hogent/androidproject/data/DataSource.kt
@@ -0,0 +1,22 @@
+package com.hogent.androidproject.data
+
+import com.hogent.androidproject.model.Game
+
+class DataSource() {
+    fun loadGames(): List<Game> {
+        return listOf<Game>(
+            Game("Overwatch 2",
+                "A hero-focused first-person team shooter from Blizzard Entertainment.",
+                "Shooter",
+                "PC (Windows)",
+                "Activision Blizzard"
+                ) ,
+            Game("PUBG: BATTLEGROUNDS",
+            "Get into the action in one of the longest running battle royale games PUBG Battlegrounds.",
+            "Shooter",
+            "PC (Windows)",
+            "KRAFTON, Inc."
+        )
+        )
+    }
+}

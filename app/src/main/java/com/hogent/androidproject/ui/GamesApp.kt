7239a35c396de9435b7673ba6b7e849cebe3e90c
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-11-07 11:09:38 +0100

    Refactoring of GamesApp

--- app/src/main/java/com/hogent/androidproject/ui/GamesApp.kt
+++ app/src/main/java/com/hogent/androidproject/ui/GamesApp.kt
@@ -1,17 +1,11 @@
 package com.hogent.androidproject.ui
 
-import androidx.annotation.StringRes
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -19,13 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -99,7 +87,7 @@ fun GamesApp(viewModel: GameViewModel = viewModel(), navController: NavHostContr
     }
 }
 /**
- * Animatie voor binnenkomen scherm
+ * Animation for screen that enters
  */
 fun enterScreen() : EnterTransition {
     return slideInHorizontally(

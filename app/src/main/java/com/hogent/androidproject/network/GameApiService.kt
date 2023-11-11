b30331f00a84c4602d0b173d8d6d015ff9031114
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-11-11 15:03:02 +0100

    Added singleton object for retrofitservice

--- app/src/main/java/com/hogent/androidproject/network/GameApiService.kt
+++ app/src/main/java/com/hogent/androidproject/network/GameApiService.kt
@@ -15,3 +15,9 @@ interface GameApiService {
     @GET("games")
     fun getGames(): String
 }
+
+object GameApi {
+    val retrofitService: GameApiService by lazy {
+        retrofit.create(GameApiService::class.java)
+    }
+}

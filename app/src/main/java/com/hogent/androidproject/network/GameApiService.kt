024218b0286c6fc41f0f6d39758461ccbe8ad3f9
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-11-11 14:58:44 +0100

    Created GameApiService

---
+++ app/src/main/java/com/hogent/androidproject/network/GameApiService.kt
@@ -0,0 +1,17 @@
+package com.hogent.androidproject.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+    "https://www.freetogame.com/api"
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface GameApiService {
+    @GET("games")
+    fun getGames(): String
+}

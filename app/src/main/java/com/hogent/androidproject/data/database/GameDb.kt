de72ef2c4e90d86ef0ee80dfe9a998707887674a
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-11-26 13:27:53 +0100

    Created GameDb class

---
+++ app/src/main/java/com/hogent/androidproject/data/database/GameDb.kt
@@ -0,0 +1,30 @@
+package com.hogent.androidproject.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Database class of games
+ */
+@Database(entities = [dbGame::class], version=1, exportSchema = false)
+abstract class GameDb : RoomDatabase() {
+
+    abstract fun gameDao(): GameDao
+
+    companion object {
+        @Volatile
+        private var Instance: GameDb? = null
+
+        fun getDatabase(context: Context): GameDb {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, GameDb::class.java, "game_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}

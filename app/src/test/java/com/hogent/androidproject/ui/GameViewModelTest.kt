df31144c4fdd7fe6dfd4536f1b392ebd34a4bf10
QuintenDW <quinten.dewolf2@student.hogent.be>
2023-11-26 12:13:05 +0100

    Created tests for repository

--- app/src/test/java/com/hogent/androidproject/ui/GameViewModelTest.kt
+++ app/src/test/java/com/hogent/androidproject/ui/GameViewModelTest.kt
@@ -1,13 +1,27 @@
 package com.hogent.androidproject.ui
 
+import com.hogent.androidproject.fake.FakeApiGameRepository
+import com.hogent.androidproject.fake.FakeDataSource
+import com.hogent.androidproject.network.asDomainObjects
+import com.hogent.androidproject.ui.home.GameApiState
 import com.hogent.androidproject.ui.home.GameViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
 class GameViewModelTest {
-    private val viewModel = GameViewModel()
+    private val viewModel = GameViewModel(gameRepository = FakeApiGameRepository())
 
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
     @Test
     fun `Viewmodel starts with default values`() {
         assertEquals("PC",viewModel.gameUiState.value.platform)
@@ -26,4 +40,20 @@ class GameViewModelTest {
         assertEquals("mmorpg",viewModel.gameUiState.value.category)
     }
 
+    @Test
+    fun `Can create games list`() {
+        viewModel.createGameList()
+        assertEquals(FakeDataSource.games.asDomainObjects(),viewModel.gameUiState.value.gameList)
+        assertEquals(GameApiState.Success(viewModel.gameUiState.value.gameList),viewModel.gameApiState)
+    }
+}
+
+class  TestDispatcherRule(val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
 }
